3e8c60c46ef475ca0310610430852b2e05bf89ce
Krishna Suhagiya <krishna.suhagiya@colorado.edu>
2024-02-25 01:10:30 -0700

    Assignment 5_2: Change the assignment-3 commit ID to build against
    Signed-off-by: Krishna Suhagiya <krishna.suhagiya@colorado.edu>

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = ed9568f0e602601f14124ea23a630cd0ba56ac00
+AESD_ASSIGNMENTS_VERSION = d0a13267e2cb3453ff93f0ff033a74d1816d57b1
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
