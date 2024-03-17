f468595d7bc8b762cd254154c3ff55cdbabb378a
Krishna Suhagiya <krishna.suhagiya@colorado.edu>
2024-03-17 00:51:10 -0600

    Assignment 8: Install aesd applications and startup scripts
    Signed-off-by: Krishna Suhagiya <krishna.suhagiya@colorado.edu>

---
+++ base_external/package/aesdchar/aesdchar.mk
@@ -0,0 +1,26 @@
+
+##############################################################
+#
+# AESD_CHAR_DRIVER
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 8 git contents
+AESD_CHAR_DRIVER_VERSION = 48c872c2aefc964268a364d7f749d6fe06cf00bb
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_CHAR_DRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-krishnasuhagiya29.git
+AESD_CHAR_DRIVER_SITE_METHOD = git
+AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
+
+AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
+
+define AESD_CHAR_DRIVER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdchar
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
