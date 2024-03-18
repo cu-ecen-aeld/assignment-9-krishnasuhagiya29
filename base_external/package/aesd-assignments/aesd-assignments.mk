9ccbd13d1deb9dc0d83304c1f09fb6a2bebe3903
Krishna Suhagiya <krishna.suhagiya@colorado.edu>
2024-03-17 21:37:00 -0600

    Assignment 8: Use the latest commit ID
    Signed-off-by: Krishna Suhagiya <krishna.suhagiya@colorado.edu>

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = b2316154d2cbd57b36a2e7a9867ad3a411eeb999
+AESD_ASSIGNMENTS_VERSION = 7ed92ddff4613f38e699dc17f97dfa9499928d45
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
