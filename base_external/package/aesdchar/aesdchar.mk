e645ba45f8393f8eec7226a1cce0c9a2f2fd9d93
Krishna Suhagiya <krishna.suhagiya@colorado.edu>
2024-03-31 17:20:52 -0600

    Assignment 9: Use latest commit ID
    Signed-off-by: Krishna Suhagiya <krishna.suhagiya@colorado.edu>

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 8 git contents
-AESDCHAR_VERSION = 7ed92ddff4613f38e699dc17f97dfa9499928d45
+AESDCHAR_VERSION = 3b9652cf2f94226819c8844409c9370db936cb2a
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
